0c449df547d20f4c40d8fdd9ac76d17e14b7d2a7
Bertrand B. <boyer.bertrand@gmail.com>
2023-11-06 14:47:29 +0100

    Add missing angle brackets on `List` initialization.

--- src/main/java/ai/gleamer/game/rewrite/Game.java
+++ src/main/java/ai/gleamer/game/rewrite/Game.java
@@ -5,15 +5,15 @@ import java.util.LinkedList;
 
 public class Game {
 
-    ArrayList players = new ArrayList();
+    ArrayList players = new ArrayList<>();
     int[] places = new int[6];
     int[] purses = new int[6];
     boolean[] inPenaltyBox = new boolean[6];
 
-    LinkedList popQuestions = new LinkedList();
-    LinkedList scienceQuestions = new LinkedList();
-    LinkedList sportsQuestions = new LinkedList();
-    LinkedList rockQuestions = new LinkedList();
+    LinkedList popQuestions = new LinkedList<>();
+    LinkedList scienceQuestions = new LinkedList<>();
+    LinkedList sportsQuestions = new LinkedList<>();
+    LinkedList rockQuestions = new LinkedList<>();
 
     int currentPlayer = 0;
     boolean isGettingOutOfPenaltyBox;
