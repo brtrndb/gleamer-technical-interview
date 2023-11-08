85967e4ba2bec3f6fbe902d0c2e4d54e79fdd665
Bertrand B. <boyer.bertrand@gmail.com>
2023-11-08 15:13:13 +0100

    Create classe `QuestionsDeck` to manipulate questions.

---
+++ src/main/java/ai/gleamer/game/rewrite/QuestionsDeck.java
@@ -0,0 +1,38 @@
+package ai.gleamer.game.rewrite;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class QuestionsDeck {
+
+    private final List<String> popQuestions;
+    private final List<String> scienceQuestions;
+    private final List<String> sportsQuestions;
+    private final List<String> rockQuestions;
+
+    public QuestionsDeck(int nbCardsPerCategory) {
+        this.popQuestions = new LinkedList<>();
+        this.scienceQuestions = new LinkedList<>();
+        this.sportsQuestions = new LinkedList<>();
+        this.rockQuestions = new LinkedList<>();
+
+        for (int i = 0; i < nbCardsPerCategory; i++) {
+            this.popQuestions.addLast(QuestionsBuilder.createPopQuestion(i));
+            this.scienceQuestions.addLast(QuestionsBuilder.createScienceQuestion(i));
+            this.sportsQuestions.addLast(QuestionsBuilder.createSportsQuestion(i));
+            this.rockQuestions.addLast(QuestionsBuilder.createRockQuestion(i));
+        }
+    }
+
+    public String pickQuestion(Category category) {
+        List<String> questions = switch (category) {
+            case POP -> this.popQuestions;
+            case SCIENCE -> this.scienceQuestions;
+            case SPORTS -> this.sportsQuestions;
+            case ROCK -> this.rockQuestions;
+        };
+
+        return questions.removeFirst();
+    }
+
+}
